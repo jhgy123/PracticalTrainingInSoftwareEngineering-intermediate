1da45a41d89725cafcacdd8f623616b09359f412
gyhongjie <3026412424@qq.com>
2022-09-30 19:20:11 +0800

    create elmServer module

---
+++ elmServer/src/main/java/com/example/elmserver/ElmServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.elmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmServerApplication.class, args);
+    }
+
+}
---
+++ elmServer/src/test/java/com/example/elmserver/ElmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
